29ec0b14bf66c7f392167201f8ea335df5852f06
csura.utec <2505712023@users.noreply.github.com>
2024-03-13 21:03:23 -0600

    Ejercicios 2 y 4

--- Guia_2/Guia_2_Ejercicio_2/Ejercicio_2.cs
+++ Guia_2/Guia_2_Ejercicio_2/Ejercicio_2.cs
@@ -11,3 +11,51 @@
 
     ** Valide que el contenido dentro del arraylist y el ingresado este en: Mayúsculas.
 */
+
+using System.Collections;
+
+ArrayList palabras = new ArrayList();
+
+for (int i = 1; i <= 7; i++)
+{
+    Console.Write("Ingrese la palabra {0} del ArrayList: ", i);
+    palabras.Add(Console.ReadLine().ToUpper());
+}
+
+Console.WriteLine("\nArrayList ingresado exitosamente:");
+foreach (string palabra in palabras)
+{
+    Console.WriteLine("- {0}", palabra);
+}
+
+Console.Write("Presione cualquier tacla para continuar...");
+Console.ReadKey();
+Console.Clear();
+
+Console.Write("Ingrese la palabra que desea buscar en el ArrayList: ");
+string expresion = Console.ReadLine().ToUpper();
+
+if (palabras.Contains(expresion))
+{
+    Console.WriteLine("\nLa palabra \"{0}\" si existe en el ArrayList.", expresion);
+    Console.Write("Ingrese la palabra por la cual desea reemplazar \"{0}\": ", expresion);
+
+    palabras[palabras.IndexOf(expresion)] = Console.ReadLine().ToUpper();
+
+    Console.WriteLine("\nDatos actualizados:");
+    foreach (string palabra in palabras)
+    {
+        Console.WriteLine("- {0}", palabra);
+    }
+}
+else
+{
+    Console.WriteLine("\nLa palabra \"{0}\" no existe en el ArrayList.", expresion);
+
+    Console.WriteLine("\nNo se realizaron cambios:");
+    foreach (string palabra in palabras)
+    {
+        Console.WriteLine("- {0}", palabra);
+    }
+}
+Console.ReadKey();
