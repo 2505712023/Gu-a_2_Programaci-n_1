ca2712b25229579cbb4607b48228f4d4213dc298
csura.utec <2505712023@users.noreply.github.com>
2024-03-13 21:03:23 -0600

    Ejercicios 2 y 4

--- Guia_2/Guia_2_Ejercicio_1/Ejercicio_1.cs
+++ Guia_2/Guia_2_Ejercicio_1/Ejercicio_1.cs
@@ -14,3 +14,40 @@
         ➢ Valide que lo ingresado por el usuario sean números para evitar posibles errores
             en el código.
 */
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+
+        Console.WriteLine("Ingrese 5 número enteros: ");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Número {i + 1}: ");
+            array1[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            array2[i] = array1[i] * 3;
+        }
+
+        Console.WriteLine("\nPrimer Array:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(array1[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("\nSegundo Array (Triple de los números del primer array):");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(array2[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
--- Guia_2/Guia_2_Ejercicio_4/Ejercicio_4.cs
+++ Guia_2/Guia_2_Ejercicio_4/Ejercicio_4.cs
@@ -7,3 +7,45 @@
         ➢ El número del medio y la posición que ocupa.
         ➢ El número menor y la posición que ocupa.
  */
+
+List<int> enteros = new List<int>();
+
+Console.WriteLine("Ingrese los 5 números enteros que compondrán la lista.");
+
+for (int i = 1; i <= 5; i++)
+{
+    switch (i)
+    {
+        case 1:
+            Console.Write("Ingrese el primer número: ");
+            enteros.Add(int.Parse(Console.ReadLine()));
+            break;
+        case 2:
+            Console.Write("Ingrese el segundo número: ");
+            enteros.Add(int.Parse(Console.ReadLine()));
+            break;
+        case 3:
+            Console.Write("Ingrese el tercer número: ");
+            enteros.Add(int.Parse(Console.ReadLine()));
+            break;
+        case 4:
+            Console.Write("Ingrese el cuarto número: ");
+            enteros.Add(int.Parse(Console.ReadLine()));
+            break;
+        case 5:
+            Console.Write("Ingrese el quinto número: ");
+            enteros.Add(int.Parse(Console.ReadLine()));
+            break;
+        default:
+            break;
+    }
+}
+
+List<int> enteros_ordenados = enteros.ToList();
+enteros_ordenados.Sort();
+
+Console.WriteLine("\nEl número mayor de la lista es {0} y ocupa la posición número {1}", enteros_ordenados[4], enteros.IndexOf(enteros_ordenados[4]) + 1);
+Console.WriteLine("El número del medio de la lista es {0} y ocupa la posición número {1}", enteros_ordenados[2], enteros.IndexOf(enteros_ordenados[2]) + 1);
+Console.WriteLine("El número menor de la lista es {0} y ocupa la posición número {1}", enteros_ordenados[0], enteros.IndexOf(enteros_ordenados[0]) + 1);
+
+Console.ReadKey();
